6dcb43190a694de943333415c8553aef1a380887
Sergej Jaskiewicz <jaskiewiczs@icloud.com>
2025-12-06 22:58:07 +0100

    friends.areFriends request

--- codegen/Sources/Schema/Methods/Friends.swift
+++ codegen/Sources/Schema/Methods/Friends.swift
@@ -42,4 +42,53 @@ let friends = Group("Friends") {
 		Returns the identifier of the newly created friend list.
 		""")
 	.requiresPermissions("friends")
+
+	let friendshipInfo = StructDef("FriendshipInfo") {
+		FieldDef("user_id", type: .def(userID))
+			.required()
+			.doc("User identifier.")
+		let state = EnumDef<String>("State") {
+			EnumCaseDef("none", additionalRepresentation: 0)
+				.doc("No relationship.")
+			EnumCaseDef("following", additionalRepresentation: 1)
+				.doc("Current user is following this user.")
+			EnumCaseDef("followed_by", additionalRepresentation: 2)
+				.doc("This user is following the current user.")
+			EnumCaseDef("friends", additionalRepresentation: 3)
+				.doc("Users are friends (they follow each other).")
+			EnumCaseDef("follow_requested")
+				.doc("""
+					Only for remote users – current user tried to follow this
+					user, but their server hasn’t yet accepted that request
+					""")
+		}
+		FieldDef("friend_status", type: .def(state))
+			.required()
+			.doc("The relationship between this user and the current user.")
+		state
+		FieldDef("is_request_unread", type: .bool)
+			.doc("""
+				Whether there’s an incoming friend request from this user.
+				Only returned when ``extended`` is `true` and ``friendStatus``
+				is ``followedBy``.
+				""")
+	}
+	RequestDef("friends.areFriends", resultType: .array(.def(friendshipInfo))) {
+		FieldDef("user_ids", type: .array(.def(userID)))
+			.required()
+			.doc("""
+				The list of user identifiers to retrieve friendship states for.
+				""")
+		
+		FieldDef("extended", type: .bool)
+			.required()
+			.doc("Whether to return ``FriendshipInfo/isRequestUnread``.")
+		
+		friendshipInfo
+	}
+	.doc("""
+		Returns information about friendship states and friend requests related
+		to the specified users.
+		""")
+	.requiresPermissions("friends:read")
 }

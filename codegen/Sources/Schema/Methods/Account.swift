c1315185636aeb03f13af23ac8afb1719691ac31
Sergej Jaskiewicz <jaskiewiczs@icloud.com>
2025-12-23 02:27:00 +0100

    account.getCounters method

--- codegen/Sources/Schema/Methods/Account.swift
+++ codegen/Sources/Schema/Methods/Account.swift
@@ -5,4 +5,35 @@ let account = Group("Account") {
 	) {
 	}
 	.doc("Returns the list of permissions granted to the access token.")
+
+	RequestDef("account.getCounters") {
+		StructDef("Result") {
+			FieldDef("friends", type: .int)
+				.required()
+				.doc("New friend requests.")
+			FieldDef("notifications", type: .int)
+				.required()
+				.doc("""
+					New notifications (“feedback”).
+					Whether or not this includes likes and reposts is
+					determined by the corresponding setting.
+					""")
+			FieldDef("groups", type: .int)
+				.required()
+				.doc("New group invitations.")
+			FieldDef("events", type: .int)
+				.required()
+				.doc("New event invitations.")
+			FieldDef("messages", type: .int)
+				.required()
+				.doc("Unread messages.")
+			FieldDef("photos", type: .int)
+				.required()
+				.doc("New photo tags.")
+		}
+	}
+	.doc("""
+		Returns how many of each type of unread items the current user
+		has.
+		""")
 }

aeea43df6ccd581d10008a90088c38941e2fb485
Sergej Jaskiewicz <jaskiewiczs@icloud.com>
2026-01-02 17:32:19 +0100

    Add a URLRequest initializer for OAuth requests

--- Sources/SmithereenAPI/Networking.swift
+++ Sources/SmithereenAPI/Networking.swift
@@ -11,6 +11,35 @@ private let urlFormEncoder = URLEncodedFormEncoder(
 )
 
 extension URLRequest {
+	private init<Request: EncodableRequestProtocol>(
+		host: String,
+		request: Request,
+		cachePolicy: URLRequest.CachePolicy = .useProtocolCachePolicy,
+		timeoutInterval: TimeInterval = 60.0,
+		additionalQueryItems: (inout [URLQueryItem]) throws -> Void,
+	) throws {
+		var queryItems = [URLQueryItem]()
+		try additionalQueryItems(&queryItems)
+		if let query = request.encodableQuery {
+			try urlFormEncoder.encode(query, into: &queryItems)
+		}
+		var urlComponents = URLComponents()
+		urlComponents.scheme = "https"
+		urlComponents.host = host
+		urlComponents.path = request.path
+		urlComponents.queryItems = queryItems
+		guard let url = urlComponents.url else {
+			fatalError("Could not create a URL for the method: \(urlComponents)")
+		}
+		self.init(url: url, cachePolicy: cachePolicy, timeoutInterval: timeoutInterval)
+		httpMethod = Request.method.rawValue
+		if let body = request.encodableBody {
+			addValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+			httpBody = try Data(urlFormEncoder.encode(body).utf8)
+		}
+		addValue("application/json", forHTTPHeaderField: "Accept")
+	}
+
 	/// Creates a ready-to-use URL request for the provided Smithereen API request.
 	///
 	/// `globalParameters` are always passed in the query part.
@@ -38,28 +67,39 @@ extension URLRequest {
 		if passAccessTokenInHeader {
 			globalParameters.accessToken = nil
 		}
-		var queryItems = [URLQueryItem]()
-		try urlFormEncoder.encode(globalParameters, into: &queryItems)
-		if let query = request.encodableQuery {
-			try urlFormEncoder.encode(query, into: &queryItems)
+		try self.init(
+			host: host,
+			request: request,
+			cachePolicy: cachePolicy,
+			timeoutInterval: timeoutInterval,
+		) { queryItems in
+			try urlFormEncoder.encode(globalParameters, into: &queryItems)
 		}
-		var urlComponents = URLComponents()
-		urlComponents.scheme = "https"
-		urlComponents.host = host
-		urlComponents.path = request.path
-		urlComponents.queryItems = queryItems
-		guard let url = urlComponents.url else {
-			fatalError("Could not create a URL for the method: \(urlComponents)")
-		}
-		self.init(url: url, cachePolicy: cachePolicy, timeoutInterval: timeoutInterval)
-		httpMethod = Request.method.rawValue
 		if let accessToken, passAccessTokenInHeader {
 			addValue("Bearer \(accessToken)", forHTTPHeaderField: "Authorization")
 		}
-		if let body = request.encodableBody {
-			addValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
-			httpBody = try Data(urlFormEncoder.encode(body).utf8)
-		}
-		addValue("application/json", forHTTPHeaderField: "Accept")
+	}
+
+	/// Creates a ready-to-use URL request for the provided Smithereen OAuth request.
+	///
+	/// - parameters:
+	///	  - host: The Smithereen instance server address.
+	///     Must be a valid host name.
+	///   - request: The OAuth request to perform.
+	///   - cachePolicy: The cache policy for the request.
+	///   - timeoutInterval: The timeout interval for the request. The default is 60.0.
+	public init<Request: SmithereenOAuthTokenRequest>(
+		host: String,
+		request: Request,
+		cachePolicy: URLRequest.CachePolicy = .useProtocolCachePolicy,
+		timeoutInterval: TimeInterval = 60.0,
+	) throws {
+		try self.init(
+			host: host,
+			request: request,
+			cachePolicy: cachePolicy,
+			timeoutInterval: timeoutInterval,
+			additionalQueryItems: { _ in },
+		)
 	}
 }

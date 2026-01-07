3f5a037ac64c857eaabc563fced66c22c8fd307a
Sergej Jaskiewicz <sergej.jaskiewicz@jetbrains.com>
2026-01-07 13:12:35 +0100

    Make SmithereenAPIVersion comparable

--- Sources/SmithereenAPI/SupportedAPIVersions.swift
+++ Sources/SmithereenAPI/SupportedAPIVersions.swift
@@ -1,10 +1,46 @@
+/// See [Smithereen API versions](https://smithereen.software/docs/api/versions/).
 public struct SmithereenAPIVersion: APIVersionDictionaryEntry, Hashable, Codable, Sendable {
 	public static let dictionaryKey: String = "smithereen"
-	public var rawValue: String
-	public init(rawValue: String) {
-		self.rawValue = rawValue
+
+	public var major: Int
+	public var minor: Int
+
+	public init(major: Int, minor: Int) {
+		self.major = major
+		self.minor = minor
 	}
-	public static let v1_0 = Self(rawValue: "1.0")
+
+	public var rawValue: String {
+		"\(major).\(minor)"
+	}
+
+	public init?(rawValue: String) {
+		let components = rawValue.split(
+			separator: ".",
+			maxSplits: 1,
+			omittingEmptySubsequences: false,
+		)
+		if components.count != 2 {
+			return nil
+		}
+		guard let major = Int(components[0]), let minor = Int(components[1]) else {
+			return nil
+		}
+		self.major = major
+		self.minor = minor
+	}
+
+	public static let v1_0 = Self(major: 1, minor: 0)
+}
+
+extension SmithereenAPIVersion: Comparable {
+	public static func < (lhs: Self, rhs: Self) -> Bool {
+		if lhs.major == rhs.major {
+			return lhs.minor < rhs.minor
+		} else {
+			return lhs.major < rhs.major
+		}
+	}
 }
 
 extension APIVersionDictionary {
